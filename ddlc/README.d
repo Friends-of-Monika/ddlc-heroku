312e2da623a096f7a0461ccb13b5cc16f5c0c546
dreamscached <dreamscache.d@gmail.com>
2022-01-19 14:34:08 +0100

    Create README.d

---
+++ ddlc/README.d
@@ -0,0 +1,14 @@
+# /ddlc
+
+Put your existing DDLC install contents here (this should include foldes and files such as `game` and `DDLC.exe`.)
+
+**Important!** It is necessary that DDLC install contents are located right here! Make sure your file tree looks like this:
+```
+ddlc-heroku/
+├── ...
+└── ddlc/
+    ├── ...
+    ├── game/
+    │   └── ...
+    └── DDLC.exe
+```
